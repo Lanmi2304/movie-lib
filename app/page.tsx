86717863e293b00993066a24064ccff20f391587
Milan <milanpavlovic02@icloud.com>
2025-04-12 15:29:08 +0200

    fix: update text color class for heading in page component

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className="absolute size-full bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-cover bg-center"></div>
 
       <div className="grid max-w-3xl gap-4">
-        <h1 className="to-muted from-foreground w-full bg-gradient-to-r bg-clip-text text-4xl font-extrabold text-transparent">
+        <h1 className="to-foreground/60 from-foreground w-full bg-gradient-to-r bg-clip-text text-4xl font-extrabold text-transparent">
           Millions of movies, TV shows and people to discover. Explore now.
         </h1>
         <Search className="text-white placeholder:text-white" />
