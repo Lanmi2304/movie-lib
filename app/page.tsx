64039d80fe921b6a4d298da81c78eb79edab7399
Milan <milanpavlovic02@icloud.com>
2025-04-27 19:29:35 +0200

    fix: adjust hero section styling for improved layout and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import { Trending } from "./(hero)/_components/trending-carousel";
 export default function Page() {
   return (
     <div className="flex size-full flex-col items-center justify-center py-20">
-      <div className="absolute size-full bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-cover bg-top bg-no-repeat lg:bg-contain"></div>
+      <div className="absolute inset-0 mt-10 bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-contain bg-top bg-no-repeat"></div>
       <div className="mx-auto flex max-w-4xl flex-col items-center justify-center gap-4">
-        <h1 className="to-foreground/60 from-foreground mt-20 w-full bg-gradient-to-r bg-clip-text text-2xl font-extrabold text-transparent lg:text-4xl">
+        <h1 className="to-foreground/60 from-foreground w-full bg-gradient-to-r bg-clip-text text-2xl font-extrabold text-transparent lg:mt-20 lg:text-4xl">
           Millions of movies, TV shows and people to discover. Explore now.
         </h1>
         <Search className="text-white placeholder:text-white" />
       </div>
 
-      <div className="mt-40 flex w-full flex-col gap-10">
+      <div className="mt-10 flex w-full flex-col gap-4 lg:mt-40">
         <Trending
           title="Popular Movies"
           path="https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
