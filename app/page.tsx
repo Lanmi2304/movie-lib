089faa76f44be6cb01ebeb2dcc8b1a6ef299d976
Milan <milanpavlovic02@icloud.com>
2025-04-27 19:24:24 +0200

    fix: update background styling for hero section to improve visual presentation

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Trending } from "./(hero)/_components/trending-carousel";
 
 export default function Page() {
   return (
-    <div className="flex w-full flex-col items-center justify-center py-20">
-      <div className="absolute size-full bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-contain bg-no-repeat"></div>
+    <div className="flex size-full flex-col items-center justify-center py-20">
+      <div className="absolute size-full bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-cover bg-top bg-no-repeat lg:bg-contain"></div>
       <div className="mx-auto flex max-w-4xl flex-col items-center justify-center gap-4">
         <h1 className="to-foreground/60 from-foreground mt-20 w-full bg-gradient-to-r bg-clip-text text-2xl font-extrabold text-transparent lg:text-4xl">
           Millions of movies, TV shows and people to discover. Explore now.
