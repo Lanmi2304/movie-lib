33e067fc87d4dd7700a8496a33463c7a2ff55a80
Milan <milanpavlovic02@icloud.com>
2025-05-15 15:16:05 +0200

    fix: correct layout structure in Page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { MediaCarousel } from "./(hero)/_components/media-carousel";
 
 export default function Page() {
   return (
-    <div className="flex w-full flex-col items-center justify-center py-20">
-      <div className="absolute inset-0 mt-10 bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-auto bg-top bg-no-repeat md:bg-contain"></div>
+    <div className="relative flex w-full flex-col items-center justify-center py-20">
+      <div className="absolute inset-0 bg-[url(/images/dp_hero.jpg)] mask-r-from-5% mask-l-from-5% bg-auto bg-top bg-no-repeat md:bg-contain"></div>
       <div className="mx-auto flex max-w-4xl flex-col items-center justify-center gap-4">
         <h1 className="to-foreground/60 from-foreground w-full bg-gradient-to-r bg-clip-text text-2xl font-extrabold text-transparent lg:mt-20 lg:text-4xl">
           Millions of movies, TV shows and people to discover. Explore now.
